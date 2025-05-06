ee0337760ead77a2f231d61aa74b6ceebd921944
lzx20129002 <lzx20129002@163.com>
2025-05-06 15:51:52 +0800

    3大打广

--- src/plugins/ban_advertisement/ban_advertisement.py
+++ src/plugins/ban_advertisement/ban_advertisement.py
@@ -11,6 +11,8 @@ import re
 from datetime import datetime
 from typing import Union, Iterable, Any
 
+import aiohttp
+from aiohttp import ClientSession
 from nonebot.internal.rule import Rule
 from nonebot.params import CommandArg
 from nonebot.rule import to_me
@@ -18,11 +20,11 @@ from pyzbar.pyzbar import decode, Decoded
 from PIL import Image
 from nonebot.plugin.on import on_message, on_command, on_notice, on
 from nonebot.adapters.onebot.v11 import Bot as V11Bot, GroupMessageEvent, GroupRecallNoticeEvent, GROUP_MEMBER, Event, \
-    PrivateMessageEvent
+    PrivateMessageEvent, NoticeEvent
 from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 
 from .config import Config
-from .module import fetch_image_from_url_ssl
+from .module import fetch_image_from_url_ssl, count_digits_generator, ai
 from nonebot import require, get_driver, get_plugin_config, get_bot, logger
 from nonebot.log import default_format, default_filter
 
@@ -32,6 +34,7 @@ from nonebot_plugin_apscheduler import scheduler
 config = get_plugin_config(Config)
 driver = get_driver()
 ban_logger = logger.bind(ban=True)
+aiohttp_session: ClientSession
 
 data = {
     "user_status": {}
@@ -47,52 +50,72 @@ ban_time = [
 
 @driver.on_startup
 async def startup():
+    global aiohttp_session
+    aiohttp_session = aiohttp.ClientSession()
+
     logger.add("log/log_{time:YYYY-MM-DD}.log", level=0,
                format=default_format, rotation="2 days", retention="3 weeks", enqueue=True, filter=lambda record: not "ban" in record["extra"] and default_filter(record))
     logger.add("log/ban/log_{time:YYYY-MM-DD}.log", level=0,
                format=default_format, rotation="2 days", retention="3 weeks", enqueue=True, filter=lambda record: "ban" in record["extra"] and default_filter(record))
 
+@driver.on_shutdown
+async def shutdown():
+    global aiohttp_session
+    await aiohttp_session.close()
+
 def is_allowed_group(group: Iterable) -> Rule:
     async def check_group(bot: V11Bot, event: Event) -> bool:
         return event.group_id in group
     return Rule(check_group)
-recall = on_notice(rule=is_allowed_group(config.group_id))
 
-@recall.handle()
-async def _(event: GroupRecallNoticeEvent, bot: V11Bot):
-    if event.group_id in config.group_id:
-        if event.operator_id == 2854196310:
-            await asyncio.sleep(1)
-            img_msg_data=await bot.call_api("get_msg", message_id=event.message_id)
-            for i in img_msg_data["message"]:
-                if i["type"] == "image":
-                    ban_logger.info(f"图片url -{i["data"]["url"]} 来自 {event.user_id}@[群:{event.group_id}]")
-                    img_bytesio = await fetch_image_from_url_ssl(i["data"]["url"])
-                    img = Image.open(img_bytesio)
-                    result: list[Decoded] = decode(img)
-                    if result:
-                        data["user_status"][event.user_id] = data["user_status"].get(event.user_id, 0) + 1
-                        ban_logger.info(f"解析图片url -{i["data"]["url"]} 来自 {event.user_id}@[群:{event.group_id}]] '{json.dumps([i.data.decode() for i in result])}'")
-            if ban_time[data["user_status"].get(event.user_id,0)] is True:
-                await recall.send(Message([
-                    MessageSegment.text("用户"),
-                    MessageSegment.at(event.user_id),
-                    MessageSegment.text(f"({event.user_id})警告多次无效，执行纪律")
-                ]))
-                await bot.call_api("set_group_kick", group_id=event.group_id, user_id=event.user_id, reject_add_request=True)
-                del data["user_status"][event.user_id]
-                ban_logger.info(f"操作执行 -踢出用户 来自 {event.user_id}@[群:{event.group_id}]")
-                return
-            elif ban_time[data["user_status"].get(event.user_id,0)]:
-                await recall.send(Message([
-                    MessageSegment.text("用户"),
-                    MessageSegment.at(event.user_id),
-                    # MessageSegment.text(f"({event.user_id})打广告{data["user_status"][event.user_id]}次,禁言{ban_time[data["user_status"][event.user_id]]}min")
-                    MessageSegment.text(f"({event.user_id})打广告{data["user_status"][event.user_id]}次")
-                ]))
-                await bot.call_api("set_group_ban", group_id=event.group_id, user_id=event.user_id, duration=ban_time[data["user_status"][event.user_id]]*60)
-                # logger.info(f"用户{event.user_id}尝试刷屏{data["user_status"][event.user_id]}次,禁言{ban_time[data["user_status"][event.user_id]]}min")
-                ban_logger.info(f"操作执行 -警告用户 来自 {event.user_id}@[群:{event.group_id}]")
+
+message = on_message(rule=is_allowed_group(config.group_id), permission=GROUP_MEMBER)
+
+@message.handle()
+async def _(event: GroupMessageEvent, bot: V11Bot):
+    for i in event.message["image"]:
+        ban_logger.info(f"图片url -{i.data["url"]} 来自 {event.user_id}@[群:{event.group_id}]")
+        img_bytesio = await fetch_image_from_url_ssl(i.data["url"], aiohttp_session)
+        img = Image.open(img_bytesio)
+        result: list[Decoded] = decode(img)
+        if result:
+            await bot.delete_msg(message_id=event.message_id)
+            data["user_status"][event.user_id] = data["user_status"].get(event.user_id, 0) + 1
+            ban_logger.info(f"解析图片url -{i.data["url"]} 来自 {event.user_id}@[群:{event.group_id}]] '{json.dumps([i.data.decode() for i in result])}'")
+    for i in event.message["json"]:
+        if json.loads(i.data["data"])["bizsrc"] == "qun.share":
+            await bot.delete_msg(message_id=event.message_id)
+            data["user_status"][event.user_id] = data["user_status"].get(event.user_id, 0) + 1
+            ban_logger.info(f"消息 -推荐群聊 来自 {event.user_id}@[群:{event.group_id}]")
+    if count_digits_generator(event.message.extract_plain_text()):
+        group_name = (await bot.get_group_info(group_id=event.group_id, no_cache=True))["group_name"]
+        text_message = event.message.extract_plain_text()
+        ban_logger.info(f"消息 -收到 来自 {event.user_id}@[群:{event.group_id}] {text_message}")
+        if (await ai(text_message, group_name, aiohttp_session))["is_pornographic"]:
+            await bot.delete_msg(message_id=event.message_id)
+            data["user_status"][event.user_id] = data["user_status"].get(event.user_id, 0) + 1
+            ban_logger.info(f"消息 -确定 来自 {event.user_id}@[群:{event.group_id}] {text_message}")
+        # await message.finish(str(count_digits_generator(event.message.extract_plain_text())))
+
+    if ban_time[data["user_status"].get(event.user_id,0)] is True:
+        await message.send(Message([
+            MessageSegment.text("用户"),
+            MessageSegment.at(event.user_id),
+            MessageSegment.text(f"({event.user_id})警告多次无效，执行纪律")
+        ]))
+        await bot.set_group_kick(group_id=event.group_id, user_id=event.user_id, reject_add_request=True)
+        del data["user_status"][event.user_id]
+        ban_logger.info(f"操作执行 -踢出用户 来自 {event.user_id}@[群:{event.group_id}]")
+    elif ban_time[data["user_status"].get(event.user_id,0)]:
+        await message.send(Message([
+            MessageSegment.text("用户"),
+            MessageSegment.at(event.user_id),
+            # MessageSegment.text(f"({event.user_id})打广告{data["user_status"][event.user_id]}次,禁言{ban_time[data["user_status"][event.user_id]]}min")
+            MessageSegment.text(f"({event.user_id})打广告{data["user_status"][event.user_id]}次")
+        ]))
+        await bot.set_group_ban(group_id=event.group_id, user_id=event.user_id, duration=ban_time[data["user_status"][event.user_id]]*60)
+        # logger.info(f"用户{event.user_id}尝试刷屏{data["user_status"][event.user_id]}次,禁言{ban_time[data["user_status"][event.user_id]]}min")
+        ban_logger.info(f"操作执行 -警告用户 来自 {event.user_id}@[群:{event.group_id}]")
 
 
 # log = on_command("log")
--- src/plugins/ban_advertisement/module.py
+++ src/plugins/ban_advertisement/module.py
@@ -4,16 +4,16 @@
 # @IDE     : PyCharm
 # @Author  : lzx9002
 # @Time    : 2025/4/6 14:25
+import json
 import ssl
-from datetime import datetime
-from enum import Enum
 from io import BytesIO
+from typing import TypedDict, Literal
 
 import aiohttp
-from aiomysql import create_pool, DictCursor
+from aiohttp import ClientSession
 
 
-async def fetch_image_from_url_ssl(url: str, **params) -> BytesIO | None:
+async def fetch_image_from_url_ssl(url: str, session: ClientSession) -> BytesIO | None:
 
     SSL_CONTEXT = ssl.create_default_context()
     SSL_CONTEXT.set_ciphers("DEFAULT")
@@ -21,10 +21,33 @@ async def fetch_image_from_url_ssl(url: str, **params) -> BytesIO | None:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
     }
+    async with session.get(url, ssl=SSL_CONTEXT, headers=headers) as response:
+        if response.status == 200:
+            image_data = BytesIO(await response.read())
+            return image_data
+        return None
+def count_digits_generator(s: str, min_digits: int=4):
+    return sum(1 for char in s if char.isdigit()) >= min_digits
 
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url, **params, ssl=SSL_CONTEXT) as response:
-            if response.status == 200:
-                image_data = BytesIO(await response.read())
-                return image_data
-            return None
+async def ai(message: str, group_name: str, session: ClientSession) -> dict[Literal["is_pornographic"], bool]:
+    headers={"Authorization": f"Bearer sk-rvitzpixehecvqxxrdipetjnzcxobqjvwbepkveudexesgkn","Content-Type": "application/json"}
+    payload = {
+        "model": "deepseek-ai/DeepSeek-V3",
+        "messages": [
+            {
+                "role": "system",
+                "content": f"\"{message}\"帮我判断是不是色情内容(嫖娼广告等),数字为群号，群号搜索到的群名称是\"{group_name}\",返回{{\"is_pornographic\": true/false}}]"
+            }
+        ],
+        "stream": False,
+        "stop": None,
+        "response_format": {"type": "json_object"},
+    }
+    async with session.post("https://api.siliconflow.cn/v1/chat/completions", json=payload, headers=headers) as response:
+        # data = await response.read()
+        data = await response.json()
+        return json.loads(data["choices"][0]["message"]["content"])
+
+# class aiReturn(TypedDict):
+
+
